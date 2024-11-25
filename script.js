b0cfd5d0e29941b5858d7c2bc21d75d81182bdc1
mentipento <26530260+mentipento@users.noreply.github.com>
2024-11-25 09:55:05 +0100

    create basic math functions add(), subtract(), multiply() and divide()

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(num1, num2) {
+    return num1 + numn2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    if (num2 === 0) {
+        return "shame on you!"
+    } else {
+        return num1 / num2;
+    }
+
+}
